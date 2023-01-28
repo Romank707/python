54002151cecc862fe3af2dc807c57b10da46bd91
Romank707 <122994879+Romank707@users.noreply.github.com>
2023-01-28 18:31:03 +0300

    lesson-4

---
+++ 4.2.py
@@ -0,0 +1,51 @@
+my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+
+# вариант 1
+
+less_list = [num1 for num1, num2 in zip(my_list[1:], my_list[:-1]) if num1 > num2]
+
+# вариант 2 c enumerate
+
+less_list = [number for i, number in enumerate(my_list) if i > 0 and my_list[i] > my_list[i - 1]]
+
+print(less_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
